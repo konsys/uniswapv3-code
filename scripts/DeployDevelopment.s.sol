db2e2ffb6c084fd9fc232d1a9bf608fad5168af7
Mona Lisa <Mona@Lisa.ru>
2024-09-17 13:28:45 +0300

    fix

--- scripts/DeployDevelopment.s.sol
+++ scripts/DeployDevelopment.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-import "forge-std/console.sol";
-import "forge-std/Script.sol";
+// import "forge-std/console.sol";
+// import "forge-std/Script.sol";
 import "../src/UniswapV3Pool.sol";
 import "../src/UniswapV3Manager.sol";
 import "../test/ERC20Mintable.sol";
@@ -38,3 +38,10 @@ contract DeployDevelopment is Script {
         console.log("Manager address", address(manager));
     }
 }
+
+//   WETH address 0x5FbDB2315678afecb367f032d93F642f64180aa3
+//   USDC address 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+//   Pool address 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+//   Manager address 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
+
+// p qwertyqwerty
