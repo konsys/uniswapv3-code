ce8bf414ef7dc284ed059f8533466b5b4010c64e
Mona Lisa <Mona@Lisa.ru>
2024-09-19 20:00:11 +0300

    fix

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -174,6 +174,40 @@ contract UniswapV3Pool {
         );
     }
 
+    function swapOld(
+        address recipient,
+        bytes calldata data
+    ) public returns (int256 amount0, int256 amount1) {
+        int24 nextTick = 85184;
+        uint160 nextPrice = 5604469350942327889444743441197;
+
+        amount0 = -0.008396714242162444 ether;
+        amount1 = 42 ether;
+
+        (slot0.tick, slot0.sqrtPriceX96) = (nextTick, nextPrice);
+
+        IERC20(token0).transfer(recipient, uint256(-amount0));
+
+        uint256 balance1Before = balance1();
+        IUniswapV3SwapCallback(msg.sender).uniswapV3SwapCallback(
+            amount0,
+            amount1,
+            data
+        );
+        if (balance1Before + uint256(amount1) > balance1())
+            revert InsufficientInputAmount();
+
+        emit Swap(
+            msg.sender,
+            recipient,
+            amount0,
+            amount1,
+            slot0.sqrtPriceX96,
+            liquidity,
+            slot0.tick
+        );
+    }
+
     function swap(
         address recipient,
         bool zeroForOne,
